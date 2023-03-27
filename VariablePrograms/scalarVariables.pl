bcecf2af9826bb2b85d8a13d2138a07185c8fc3c
Himanshu-kumar-gupta <himanshukumargupta135@gmail.com>
2023-03-27 23:08:41 +0530

    Added Scalar Variables

---
+++ VariablePrograms/scalarVariables.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+$name1="Himanshu";  #String assignment
+$age1=20;           #Integer assignment
+$salary1=1000.56;    #Floating point/Decimal
+
+print "Age = $age1\n";
+print "Name = $name1\n";
+print "Salary = $salary1\n";
