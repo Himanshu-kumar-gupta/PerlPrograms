801b66afb5d11243a1771148d228811ea8beb5ee
Himanshu-kumar-gupta <himanshukumargupta135@gmail.com>
2023-03-27 23:55:04 +0530

    Added Array Variables

---
+++ VariablePrograms/arrayValues.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+@names=("Mohan","Ekansh","Himanshu");  #array start with @
+@ages=(33,34,10);
+
+print "\n\$ages[0] = $ages[0]\n";  # \$ is escape sequence
+print "\$ages[1] = $ages[1]\n";
+print "\$ages[2] = $ages[2]\n\n";
+
+print "\$names[0] = $names[0]\n";
+print "\$names[1] = $names[1]\n";
+print "\$names[2] = $names[2]\n";
