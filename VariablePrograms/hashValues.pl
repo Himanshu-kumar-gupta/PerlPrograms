cf702dee9639c4496ecf46c8aeeec6e398031c28
Himanshu-kumar-gupta <himanshukumargupta135@gmail.com>
2023-03-28 00:10:44 +0530

    Added Hash Variables

---
+++ VariablePrograms/hashValues.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+# Hashes are like maps
+# Used with % sign
+
+%user_data = ('Danish',23,'Raj',78,'Him',67); #Enter data with key,value
+
+print "\$user_data{'Danish'} = $user_data{'Danish'}\n"; #Access data with key
+print "\$user_data{'Him'} = $user_data{'Him'}\n";
+
