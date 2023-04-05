545d8cd6a5a9d5f5f8da78e3ca0067db34f863b3
Himanshu-kumar-gupta <himanshukumargupta135@gmail.com>
2023-04-05 12:14:46 +0530

    String concatenation

---
+++ stringOperation.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+$firstName = "Himanshu";
+$midName = "Kumar";
+$surName = "Gupta";
+# Concatenation
+$fullName = $firstName . " " . $midName ." ".$surName;
+
+print "\n$fullName\n";
