34da483a032bd7f2841cb59319b8f8acea5d9c2c
Himanshu-kumar-gupta <himanshukumargupta135@gmail.com>
2023-04-05 22:56:20 +0530

    Other way of adding hash elements

--- VariablePrograms/hashValues.pl
+++ VariablePrograms/hashValues.pl
@@ -4,7 +4,8 @@
 # Used with % sign
 
 %user_data = ('Danish',23,'Raj',78,'Him',67); #Enter data with key,value
-
+%hash1 = (key1 => 'val1', key2=>'val2');  #Enter data with key=>val
 print "\$user_data{'Danish'} = $user_data{'Danish'}\n"; #Access data with key
 print "\$user_data{'Him'} = $user_data{'Him'}\n";
+print "\$hash1{'key1'} = $hash1{'key1'}\n";
 
