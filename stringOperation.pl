75ea3092a92c9fdd2d5eeafd002b87f71e2379fd
Himanshu-kumar-gupta <himanshukumargupta135@gmail.com>
2023-04-05 12:33:23 +0530

    Multiply Operation on string (*)

--- stringOperation.pl
+++ stringOperation.pl
@@ -3,7 +3,18 @@
 $firstName = "Himanshu";
 $midName = "Kumar";
 $surName = "Gupta";
-# Concatenation
-$fullName = $firstName . " " . $midName ." ".$surName;
 
-print "\n$fullName\n";
+# Concatenation
+$fullName = $firstName . " " . $midName ." ".$surName;    #use .
+print "\n$fullName\n\n";
+
+
+
+# Multiplication of string
+# Results in 0, as no numbers are found at starting of string
+print ($firstName * 2);
+
+# Results in 314 i.e. 157*2, so it parses till it finds integer 
+$stringMul = "157p98a";
+$res = $stringMul*2;
+print "\n$res\n";
