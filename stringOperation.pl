4d3a0c0541b401999fe84ef60714e54296429852
Himanshu-kumar-gupta <himanshukumargupta135@gmail.com>
2023-04-05 12:42:34 +0530

    Repeat Operation

--- stringOperation.pl
+++ stringOperation.pl
@@ -17,4 +17,12 @@ print ($firstName * 2);
 # Results in 314 i.e. 157*2, so it parses till it finds integer 
 $stringMul = "157p98a";
 $res = $stringMul*2;
-print "\n$res\n";
+print "\n$res\n\n";
+
+
+
+#Repeat Operation
+$repeat1 = "qt" x 5;  #use x to repeat
+$repeat2 = ($firstName." ") x 3;  #use () to group for order of operation
+print $repeat1;
+print "\n$repeat2\n";
