ccb0f314ef382aaccb8f221d535998acc9bc894d
Himanshu-kumar-gupta <himanshukumargupta135@gmail.com>
2023-04-08 00:03:37 +0530

    Switch case

--- conditionalStatement.pl
+++ conditionalStatement.pl
@@ -43,3 +43,23 @@ $ test = 2 if 2 > 1;
 $test1 = 2 if 2 == 1;
 $test2 = 1 unless 1 > 2;
 print "\n test = $test    test1 = $test1    test2 = $test2 \n";
+
+
+
+# Switch case
+# Linux mint install - sudo apt-get install libswitch-perl
+use Switch;
+
+$var = 30;
+@array = (20,30,40);
+%hash = (56 => 90, 89 => 80);
+
+switch($var) {  # if any block gets executed the switch statement is terminated
+    case 10  {print "Number is 10"}
+    case "a" {print "string a"}
+    case [1..10,42] {print "number in discontinuous list"}
+    case (\@array) {print "number in array list"}
+    case (\%hash)  {print "entry in hash"} # compares to keys
+    else     {print "none true"}
+}
+# prints - number in array list
